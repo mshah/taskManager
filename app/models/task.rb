baff123cc1f7b8587d126790e64478816be8832e
Meena Shah <meena.shah@gmail.com>
2014-04-01 00:23:54 -0700

    add validation on date

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,4 +3,6 @@ class Task < ActiveRecord::Base
 	belongs_to :goal
 
 	validates_format_of :progress, :with => /\A(?:(NotStarted)|(NeedsWork)|(OnTrack)|(Complete))\Z/, :on => :create
+	validates :dueDate, :presence => true
+
 end
