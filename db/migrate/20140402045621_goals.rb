a41e643a24bef00a720ce6b65dff30997fa1190a
Meena Shah <meena.shah@gmail.com>
2014-04-02 23:37:06 -0700

    fix for heroku db migration

--- db/migrate/20140402045621_goals.rb
+++ db/migrate/20140402045621_goals.rb
@@ -2,8 +2,8 @@ class Goals < ActiveRecord::Migration
   def change
     reversible do |dir|
       change_table :goals do |t|
-        dir.up   { t.change :progress, :integer :default => 0}
-        dir.down { t.change :progress, :string  :default => "Complete"}
+        dir.up   { t.change :progress, :integer, :default => 0}
+        dir.down { t.change :progress, :string,  :default => "Complete"}
       end
     end
   end
